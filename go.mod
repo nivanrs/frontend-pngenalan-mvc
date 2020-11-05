e6f9ae19b265b191740847805651145b113518a2
Nivan Sugiantoro <nivanrs@gmail.com>
2020-11-05 18:07:43 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,14 @@
+module frontend-pngenalan-mvc
+
+go 1.15
+
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/google/go-cmp v0.5.2 // indirect
+	github.com/kr/pretty v0.1.0 // indirect
+	golang.org/x/sys v0.0.0-20200905004654-be1d3432aa8f // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+)
---
+++ main.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Antrian struct {
+	Id     string `json:"id"`
+	Status bool   `json:"status"`
+}
+
+var data []Antrian
+
+func main() {
+	router := gin.Default()
+	router.POST("/api/v1/antrian", AddAntrianHandler)
+	router.GET("/api/v1/antrian/status", GetAntrianHandler)
+	router.PUT("/api/v1/antrian/id/:idAntrian", UpdateAntrianHandler)
+	router.DELETE("/api/v1/antrian/id/:idAntrian/delete", DeleteAntrianHandler)
+	router.Run(":8000")
+}
+
+func AddAntrianHandler(c *gin.Context) { //* adalah pointer
+	flag, err := addAntrian()
+	if flag {
+		c.JSON(http.StatusOK, map[string]interface{}{
+			"status": "success",
+		})
+	} else {
+		c.JSON(http.StatusBadRequest, map[string]interface{}{
+			"status": "failed",
+			"error":  err,
+		})
+	}
+}
+
+func addAntrian() (bool, error) {
+	_, _, dataAntrian := getAntrian()
+	var Id string
+
+	if dataAntrian == nil {
+		Id = fmt.Sprintf("B-0")
+	} else {
+		Id = fmt.Sprintf("B-%d", len(dataAntrian))
+	}
+	data = append(data, Antrian{
+		Id:     Id,
+		Status: false,
+	})
+	return true, nil
+}
+
+func GetAntrianHandler(c *gin.Context) {
+
+	flag, err, resp := getAntrian()
+	if flag {
+		c.JSON(http.StatusOK, map[string]interface{}{
+			"status": "success",
+			"data":   resp,
+		})
+	} else {
+		c.JSON(http.StatusBadRequest, map[string]interface{}{
+			"status": "failed",
+			"error":  err,
+		})
+	}
+}
+
+func getAntrian() (bool, error, []Antrian) { // golang bisa mereturn banyak nilai
+	return true, nil, data
+
+}
+
+func UpdateAntrianHandler(c *gin.Context) {
+	idAntrian := c.Param("idAntrian")
+	flag, err := updateAntrian(idAntrian)
+	if flag {
+		c.JSON(http.StatusOK, map[string]interface{}{
+			"status": "success",
+		})
+	} else {
+		c.JSON(http.StatusBadRequest, map[string]interface{}{
+			"status": "failed",
+			"error":  err,
+		})
+	}
+}
+
+func updateAntrian(idAntrian string) (bool, error) {
+	for i, _ := range data {
+		if data[i].Id == idAntrian {
+			data[i].Status = true
+			break
+		}
+	}
+	return true, nil
+}
+
+func DeleteAntrianHandler(c *gin.Context) {
+	idAntrian := c.Param("idAntrian")
+	flag, err := deleteAntrian(idAntrian)
+	if flag {
+		c.JSON(http.StatusOK, map[string]interface{}{
+			"status": "success",
+		})
+	} else {
+		c.JSON(http.StatusBadRequest, map[string]interface{}{
+			"status": "failed",
+			"error":  err,
+		})
+	}
+}
+
+func deleteAntrian(idAntrian string) (bool, error) {
+	for i := range data {
+		if data[i].Id == idAntrian {
+			data = append(data[:i], data[i+1:]...)
+		}
+	}
+
+	return true, nil
+}
