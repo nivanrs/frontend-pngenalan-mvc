5d6116bef72dff8961fdf24c0bcae52e521870d5
Nivan Sugiantoro <nivanrs@gmail.com>
2020-11-06 07:35:38 +0700

    Penambahan firebase

--- go.mod
+++ go.mod
@@ -3,12 +3,18 @@ module frontend-pngenalan-mvc
 go 1.15
 
 require (
+	cloud.google.com/go/firestore v1.3.0 // indirect
+	firebase.google.com/go v3.13.0+incompatible
 	github.com/gin-gonic/gin v1.6.3
-	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/golang/protobuf v1.4.3 // indirect
 	github.com/google/go-cmp v0.5.2 // indirect
-	github.com/kr/pretty v0.1.0 // indirect
-	golang.org/x/sys v0.0.0-20200905004654-be1d3432aa8f // indirect
+	github.com/googleapis/gax-go v1.0.3 // indirect
+	github.com/kr/pretty v0.2.1 // indirect
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
+	go.opencensus.io v0.22.5 // indirect
+	golang.org/x/sys v0.0.0-20201101102859-da207088b7d1 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	google.golang.org/api v0.34.0
 	google.golang.org/protobuf v1.25.0 // indirect
-	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+	gopkg.in/check.v1 v1.0.0-20200902074654-038fdea0a05b // indirect
 )
--- main.go
+++ main.go
@@ -1,29 +1,54 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
+	"strings"
 
+	firebase "firebase.google.com/go"
+	"firebase.google.com/go/db"
 	"github.com/gin-gonic/gin"
+	"google.golang.org/api/option"
 )
 
-type Antrian struct {
-	Id     string `json:"id"`
-	Status bool   `json:"status"`
-}
+var client *db.Client
+var ctx context.Context
 
-var data []Antrian
+func init() {
+	ctx = context.Background()
+	conf := &firebase.Config{
+		DatabaseURL: "https://pengenalan-mvc-b1cbd.firebaseio.com/",
+	}
+	// Fetch the service account key JSON file contents
+	opt := option.WithCredentialsFile("firebase-admin-sdk.json")
+
+	// Initialize the app with a service account, granting admin privileges
+	app, err := firebase.NewApp(ctx, conf, opt)
+	if err != nil {
+		log.Fatalln("Error initializing app:", err)
+	}
+
+	client, err = app.Database(ctx)
+	if err != nil {
+		log.Fatalln("Error initializing database client:", err)
+	}
+}
 
 func main() {
 	router := gin.Default()
+	router.LoadHTMLGlob("views/*")
+
 	router.POST("/api/v1/antrian", AddAntrianHandler)
 	router.GET("/api/v1/antrian/status", GetAntrianHandler)
 	router.PUT("/api/v1/antrian/id/:idAntrian", UpdateAntrianHandler)
 	router.DELETE("/api/v1/antrian/id/:idAntrian/delete", DeleteAntrianHandler)
+	router.GET("/antrian", PageAntrianHandler)
 	router.Run(":8000")
 }
 
-func AddAntrianHandler(c *gin.Context) { //* adalah pointer
+func AddAntrianHandler(c *gin.Context) {
 	flag, err := addAntrian()
 	if flag {
 		c.JSON(http.StatusOK, map[string]interface{}{
@@ -37,24 +62,7 @@ func AddAntrianHandler(c *gin.Context) { //* adalah pointer
 	}
 }
 
-func addAntrian() (bool, error) {
-	_, _, dataAntrian := getAntrian()
-	var Id string
-
-	if dataAntrian == nil {
-		Id = fmt.Sprintf("B-0")
-	} else {
-		Id = fmt.Sprintf("B-%d", len(dataAntrian))
-	}
-	data = append(data, Antrian{
-		Id:     Id,
-		Status: false,
-	})
-	return true, nil
-}
-
 func GetAntrianHandler(c *gin.Context) {
-
 	flag, err, resp := getAntrian()
 	if flag {
 		c.JSON(http.StatusOK, map[string]interface{}{
@@ -67,11 +75,6 @@ func GetAntrianHandler(c *gin.Context) {
 			"error":  err,
 		})
 	}
-}
-
-func getAntrian() (bool, error, []Antrian) { // golang bisa mereturn banyak nilai
-	return true, nil, data
-
 }
 
 func UpdateAntrianHandler(c *gin.Context) {
@@ -89,16 +92,6 @@ func UpdateAntrianHandler(c *gin.Context) {
 	}
 }
 
-func updateAntrian(idAntrian string) (bool, error) {
-	for i, _ := range data {
-		if data[i].Id == idAntrian {
-			data[i].Status = true
-			break
-		}
-	}
-	return true, nil
-}
-
 func DeleteAntrianHandler(c *gin.Context) {
 	idAntrian := c.Param("idAntrian")
 	flag, err := deleteAntrian(idAntrian)
@@ -114,12 +107,94 @@ func DeleteAntrianHandler(c *gin.Context) {
 	}
 }
 
-func deleteAntrian(idAntrian string) (bool, error) {
-	for i := range data {
-		if data[i].Id == idAntrian {
-			data = append(data[:i], data[i+1:]...)
+func PageAntrianHandler(c *gin.Context) {
+	flag, err, result := getAntrian()
+	var currentAntrian map[string]interface{}
+
+	for _, item := range result {
+		if item != nil {
+			currentAntrian = item
+			break
 		}
 	}
 
+	if flag && len(result) > 0 {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"antrian": currentAntrian["id"],
+		})
+	} else {
+		c.JSON(http.StatusBadRequest, map[string]interface{}{
+			"status": "failed",
+			"error":  err,
+		})
+	}
+}
+
+func addAntrian() (bool, error) {
+	_, _, dataAntrian := getAntrian()
+	var Id string
+	var antrianRef *db.Ref
+	ref := client.NewRef("antrian")
+
+	if dataAntrian == nil {
+		Id = fmt.Sprintf("B-0")
+		antrianRef = ref.Child("0")
+	} else {
+		Id = fmt.Sprintf("B-%d", len(dataAntrian))
+		antrianRef = ref.Child(fmt.Sprintf("%d", len(dataAntrian)))
+	}
+	antrian := Antrian{
+		Id:     Id,
+		Status: false,
+	}
+	if err := antrianRef.Set(ctx, antrian); err != nil {
+		log.Fatal(err)
+		return false, err
+	}
 	return true, nil
+}
+
+func getAntrian() (bool, error, []map[string]interface{}) {
+	var data []map[string]interface{}
+	ref := client.NewRef("antrian")
+	if err := ref.Get(ctx, &data); err != nil {
+		log.Fatalln("Error reading from database:", err)
+		return false, err, nil
+	}
+
+	return true, nil, data
+}
+
+func updateAntrian(idAntrian string) (bool, error) {
+	ref := client.NewRef("antrian")
+	id := strings.Split(idAntrian, "-")
+	childRef := ref.Child(id[1])
+	antrian := Antrian{
+		Id:     idAntrian,
+		Status: true,
+	}
+	if err := childRef.Set(ctx, antrian); err != nil {
+		log.Fatal(err)
+		return false, err
+	}
+
+	return true, nil
+}
+
+func deleteAntrian(idAntrian string) (bool, error) {
+
+	ref := client.NewRef("antrian")
+	id := strings.Split(idAntrian, "-")
+	childRef := ref.Child(id[1])
+	if err := childRef.Delete(ctx); err != nil {
+		log.Fatal(err)
+		return false, err
+	}
+
+	return true, nil
+}
+
+type Antrian struct {
+	Id     string `json:"id"`
+	Status bool   `json:"status"`
 }
